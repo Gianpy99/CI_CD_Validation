28dcf9138ded77a7af812b9d32087381aa907aa2
Gianpaolo Borrello <124720503+Gianpy99@users.noreply.github.com>
2025-07-09 10:01:59 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,6 +5,7 @@ class TestAdd(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-3, 3), 0)
 
 if __name__ == "__main__":
     unittest.main()
